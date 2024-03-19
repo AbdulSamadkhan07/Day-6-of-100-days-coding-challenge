800112ab7219eb5a644c4dfbd7da7a2d91c5faca
Abdul Samad <161514467+AbdulSamadkhan07@users.noreply.github.com>
2024-03-19 05:53:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// Day 06  #100daysofcodingchallenge 
+// Question 22
+var days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+//console.log(days{7})
+console.log(days[5]);
+// Question 23
+var car = 'subaru';
+// Test 1
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test 2
+console.log("Is car != 'toyota'? I predict True.");
+console.log(car != 'toyota');
+// Test 3
+console.log("Is car === 'Covi'? I predict False.");
+console.log(car === 'covi');
+// Test 4
+console.log("Is car !== 'honda'? I predict True.");
+console.log(car !== 'honda');
+// Test 5
+console.log("Is car.startsWith('du')? I predict False.");
+console.log(car === ('du'));
+// Test 6
+console.log("Is car.includes('ba')? I predict False.");
+console.log(car === ('ba'));
+// Test 7
+var isJapanese = false;
+console.log("Is car === 'subaru' && isJapanese? I predict False.");
+console.log(car === 'subaru' && isJapanese);
+// Test 8
+var hasFourLetters = car.length === 4;
+console.log("Is car.length === 4 || HasFourLetters? I predict True.");
+console.log(car.length !== 4 || hasFourLetters);
+// Test 9
+var hasVowels = car.includes('a') || car.includes('e') || car.includes('i') || car.includes('o') || car.includes('u');
+console.log("Does car have vowels? I predict True.");
+console.log(hasVowels);
+// Test 10
+var isUpperCase = car.toUpperCase() === car;
+console.log("Is car in uppercase? I predict False.");
+console.log(isUpperCase);
